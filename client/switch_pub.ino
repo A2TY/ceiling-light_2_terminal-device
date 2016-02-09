da5faab142f74001d199a4bb11a1eddd26dda6bf
A2TY <a2ty.dev@gmail.com>
2016-02-09 23:39:43 +0900

    タクトスイッチが押されたらMQTTでPubするArduino Sketchを追加．

---
+++ client/switch_pub.ino
@@ -0,0 +1,100 @@
+/***************************************************
+  タクトスイッチが押されるとPubをするMQTTClient
+
+  https://github.com/adafruit/Adafruit_MQTT_Library
+ ****************************************************/
+
+#include <ESP8266WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "___SSID___"
+#define WLAN_PASS       "___PASS___"
+
+/***************************** Your Setup ************************************/
+
+#define YOUR_SERVER      "___ADDRESS___"
+#define YOUR_SERVERPORT  1883
+#define YOUR_USERNAME    "...your username..."
+#define YOUR_PASSWORD    "...your password..."
+
+/*************************** Global State ************************************/
+
+// MQTT serverに接続するESP8266 WiFiClientクラスを作成
+WiFiClient client;
+
+// MQTT server，クライアントID，ユーザ名，およびパスワードを保管
+const char MQTT_SERVER[] PROGMEM    = YOUR_SERVER;
+const char MQTT_CLIENTID[] PROGMEM  = __TIME__ "_test_client_id";
+const char MQTT_USERNAME[] PROGMEM  = YOUR_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = YOUR_PASSWORD;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, YOUR_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
+
+/****************************** Topic ***************************************/
+
+// pubulisher & subscriberの設定
+const char TOPIC_SWITCH[] PROGMEM = "/client/switch";
+
+Adafruit_MQTT_Publish temperaturePublisher = Adafruit_MQTT_Publish(&mqtt, TOPIC_SWITCH);
+
+/*************************** Sketch Code ************************************/
+
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(16, INPUT);
+
+  // Wi-Fiに接続
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  //WiFi.config(IPAddress(), WiFi.gatewayIP(), WiFi.subnetMask());
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  MQTT_connect();
+
+  //タクトスイッチが押されたらPub
+  if (digitalRead(16) == HIGH) {
+    Serial.println("on");
+    temperaturePublisher.publish("on");
+    delay(500);
+  }
+}
+
+// MQTT serverの接続状況を調べ切断されていれば再接続
+void MQTT_connect() {
+    int8_t ret;
+
+    // MQTT serverの接続状況を調べる
+    if (mqtt.connected()) {
+        return;
+    }
+
+    Serial.print("Connecting to MQTT... ");
+
+    while ((ret = mqtt.connect()) != 0) { // 接続された場合は0を返す　接続されない場合は5秒おきに接続を繰り返す
+        Serial.println(mqtt.connectErrorString(ret));
+        Serial.println(ret);
+        Serial.println("Retrying MQTT connection in 5 seconds...");
+        mqtt.disconnect();
+        delay(5000);  // 5秒間待機
+    }
+    Serial.println("MQTT Connected!");
+}
+
+
